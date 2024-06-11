cbca521f7fd6f3b7dfa410f4238e1dbf4ea3e159
amailemon98 <amailemon98@gmail.com>
2024-06-11 21:54:23 +0900

    react, react-router-dom

--- src/AppBack.js
+++ src/AppBack.js
@@ -8,6 +8,11 @@ import StarRatingLayer from './components/StarRating/StarRatingLayer';
 import SidebarLayout from './components/Sidebar/SidebarLayout';
 import { NavLink, Outlet, Route, Routes } from 'react-router-dom';
 import ProductsLayout from './layouts/ProductsLayout';
+import { MakeupContextProvider } from './context/MakeupContext';
+import MakeupLayout from './layouts/MakeupLayout';
+import Makeup from './routes/Makeup';
+import MakeupDetail from './routes/MakeupDetail';
+import MakeupReviews from './routes/MakeupReviews';
 
 // 3. 
 function App() { 
@@ -20,21 +25,31 @@ function App() {
                 localhost:3000/
                 localhost:3000/home
                 localhost:3000/about
-                localhost:3000/products
-                localhost:3000/products/:id
-                localhost:3000/products/:id/productDetail/:name
-                localhost:3000/products/productDetail/:name
-                localhost:3000/products/reviews
                 localhost:3000/profile
               */}
               <Route path="/"  element={<Home />} />
               <Route path="/home"  element={<Home />} />
-
+              {/* 
+                localhost:3000/products
+                localhost:3000/products/:id
+                localhost:3000/products/productDetail
+                localhost:3000/products/reviews
+              */}
               <Route  path="/products" element={ <ProductsLayout /> }>
                   <Route index  element={<Products />} />
                   <Route path=":id"  element={<ProductId />} />
                   <Route path="productDetail"  element={<ProductDetail />} />
                   <Route path="reviews"  element={<Reviews />} />
+              </Route>
+
+              <Route path='makeup' element={
+                <MakeupContextProvider>
+                  <MakeupLayout />
+                </MakeupContextProvider>
+              }>
+                <Route index element={<Makeup />} />
+                <Route path=':id' element={<MakeupDetail />} />
+                <Route path='reviews' element={<MakeupReviews />} />
               </Route>
 
 
